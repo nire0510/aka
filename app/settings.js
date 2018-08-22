8e91114d1aa0012e50e29eb132197f7181410c89
Regis Bittencourt <regisbsbittencourt@gmail.com>
2018-08-22 11:05:50 +0100

    Change home dir retriaval method

--- app/settings.js
+++ app/settings.js
@@ -4,20 +4,21 @@ var app = require('../config/app.json');
 var colors = require('colors');
 var dict = require('../config/dictionary.json');
 var path = require('path');
+var os = require('os');
 var storage = require('node-persist');
 var settings;
 
 // create & initialize storage:
 try {  
   settings = storage.create({
-    dir: path.join(process.env.HOME, app.settingsDirectoryName)
+    dir: path.join(os.homedir(), app.settingsDirectoryName)
   });
   settings.initSync();
 
   // Add setting key for private aliases if it doesn't exist:
   if (!settings.keys().some((strKey) => strKey === app.privateAliasesDirectoryPathKeyName)) {
     settings.setItemSync(app.privateAliasesDirectoryPathKeyName,
-      path.join(process.env.HOME, app.privateAliasesDirectoryName));
+      path.join(os.homedir(), app.privateAliasesDirectoryName));
   }
   // Add setting key for public aliases if it doesn't exist:
   if (!settings.keys().some((strKey) => strKey === app.publicAliasesDirectoryPathKeyName)) {
